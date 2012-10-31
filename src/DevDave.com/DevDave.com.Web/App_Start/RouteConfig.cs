ec053d7a0d57a0816c08e5845dceac9bb7765458
Dave <info@devdave.com>
2012-10-30 21:43:00 -0700

    First project commit, plenty of things to do still

---
+++ src/DevDave.com/DevDave.com.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using RouteMagic;
+
+namespace DevDave.com.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           
+            // old routes, permenant redirects
+            var homeRoute = routes.MapRoute("Home", "Home", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+            var aboutRoute = routes.MapRoute("About", "About", new { controller = "Home", action = "About", id = UrlParameter.Optional });
+            var resumeRoute = routes.MapRoute("Resume", "Resume", new { controller = "Home", action = "Resume", id = UrlParameter.Optional });
+            routes.Redirect(r => r.MapRoute("oldHome", "Home"), true).To(homeRoute);
+            routes.Redirect(r => r.MapRoute("oldAboutMe", "AboutMe"), true).To(aboutRoute);
+            routes.Redirect(r => r.MapRoute("oldAboutSite", "AboutSite"), true).To(aboutRoute);
+            routes.Redirect(r => r.MapRoute("oldResume", "Resume"), true).To(resumeRoute);
+            routes.Redirect(r => r.MapRoute("oldProjects", "Projects"), true).To(homeRoute);
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
