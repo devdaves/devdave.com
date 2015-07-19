1ec3fe03967e4365ffe56da080e2a4c1e7a4b39a
Dave Stensgaard <devdave@live.com>
2015-07-19 14:21:35 -0700

    added test webjob

---
+++ src/DevDave.com/WebjobTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebjobTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Webjob testing 123");
+        }
+    }
+}
